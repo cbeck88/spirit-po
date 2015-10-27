3a2b399f5ceda472076409d63fe01353b4de58a5
Chris Beck <beck.ct@gmail.com>
2015-10-27 14:13:37 -0400

    add copyright statements

--- include/spirit_po.hpp
+++ include/spirit_po.hpp
@@ -1,3 +1,8 @@
+//  (C) Copyright Christopher Beck
+
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include "spirit_po/version.hpp"
--- include/spirit_po/version.hpp
+++ include/spirit_po/version.hpp
@@ -1,3 +1,8 @@
+//  (C) Copyright Christopher Beck
+
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #define SPIRIT_PO_VERSION_MAJOR 0
