96061083594503c4485c9050db4f6081e8a060ce
Chris Beck <beck.ct@gmail.com>
2015-10-27 22:38:37 -0400

    travis: try to fix cxxflags being ignored

--- test_libintl/test.sh
+++ test_libintl/test.sh
@@ -6,7 +6,7 @@ rm -rf build
 mkdir build
 cd build
 
-cmake -DCMAKE_CXX_FLAGS="-O3 -Wall -Werror" ..
+cmake -DCMAKE_CXX_FLAGS="-O3 -Wall -Werror $CXXFLAGS" ..
 #cmake -DCMAKE_CXX_FLAGS="-O0 -g3 -Wall -Werror" ..
 make
 gdb -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./test_libintl
