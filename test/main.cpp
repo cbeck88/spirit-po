cee4d81611926f7f124b624779322cf6f7740e76
Chris Beck <beck.ct@gmail.com>
2015-10-31 15:34:23 -0400

    change test include order, to include boost version quickly
    give more sensible error messages if boost include is messed up

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <boost/version.hpp>
+
 #define SPIRIT_PO_NOEXCEPT
 #define SPIRIT_PO_DEBUG
 #include "spirit_po.hpp"
@@ -13,7 +15,6 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <boost/version.hpp>
 
 /***
  * Utility functions
