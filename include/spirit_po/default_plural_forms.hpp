8f43bfe0bdd2f25f3beefbebd834de50e4715ab9
Chris Beck <beck.ct@gmail.com>
2016-03-12 13:17:17 -0500

    refactor x macro for clarity

--- include/spirit_po/default_plural_forms.hpp
+++ include/spirit_po/default_plural_forms.hpp
@@ -33,7 +33,7 @@ namespace default_plural_forms {
 
 // X Macro for repetitive binary ops declarations
 
-#define SPIRIT_PO_BINARY_OPS_LIST \
+#define FOREACH_SPIRIT_PO_BINARY_OP(X_) \
  X_(and_op, &&) X_(or_op, ||) X_(eq_op, ==) X_(neq_op, !=) X_(ge_op, >=) X_(le_op, <=) X_(gt_op, >) X_(lt_op, <) X_(mod_op, %)
 
 /***
@@ -45,23 +45,24 @@ struct n_var { n_var() = default; n_var(char) {}};
 struct not_op;
 struct ternary_op;
 
-#define X_(name, op) \
+#define FWD_DECL_(name, op) \
 struct name ; \
 
-SPIRIT_PO_BINARY_OPS_LIST
-#undef X_
+FOREACH_SPIRIT_PO_BINARY_OP(FWD_DECL_)
+
+#undef FWD_DECL_
 
 /***
  * Define expr variant type
  */
 
-#define X_(name, op) boost::recursive_wrapper< name >, \
+#define WRAP_(name, op) boost::recursive_wrapper< name >, \
 
 typedef boost::variant<constant, n_var, boost::recursive_wrapper<not_op>, 
-SPIRIT_PO_BINARY_OPS_LIST
+FOREACH_SPIRIT_PO_BINARY_OP(WRAP_)
 boost::recursive_wrapper<ternary_op>> expr;
 
-#undef X_
+#undef WRAP_
 
 /***
  * Define structs
@@ -70,14 +71,15 @@ boost::recursive_wrapper<ternary_op>> expr;
 struct not_op { expr e1; };
 struct ternary_op { expr e1, e2, e3; };
 
-#define X_(name, op) \
+#define DECL_(name, op) \
 struct name { expr e1, e2; }; \
 
-SPIRIT_PO_BINARY_OPS_LIST
-#undef X_
+FOREACH_SPIRIT_PO_BINARY_OP(DECL_)
+
+#undef DECL_
 
 /***
- * Visitor that evaluates expressions
+ * Visitor that naively evaluates expressions
  */
 struct evaluator : public boost::static_visitor<uint> {
   uint n_value_;
@@ -87,11 +89,12 @@ struct evaluator : public boost::static_visitor<uint> {
   uint operator()(n_var) const { return n_value_; }
   uint operator()(const not_op & op) const { return !boost::apply_visitor(*this, op.e1); }
 
-#define X_(name, OPERATOR) \
+#define EVAL_OP_(name, OPERATOR) \
   uint operator()(const name & op) const { return (boost::apply_visitor(*this, op.e1)) OPERATOR (boost::apply_visitor(*this, op.e2)); } \
 
-SPIRIT_PO_BINARY_OPS_LIST
-#undef X_
+FOREACH_SPIRIT_PO_BINARY_OP(EVAL_OP_)
+
+#undef EVAL_OP_
 
   uint operator()(const ternary_op & op) const { return boost::apply_visitor(*this, op.e1) ? boost::apply_visitor(*this, op.e2) : boost::apply_visitor(*this, op.e3); }
 };
@@ -113,16 +116,17 @@ BOOST_FUSION_ADAPT_STRUCT(spirit_po::default_plural_forms::ternary_op,
   (spirit_po::default_plural_forms::expr, e2)
   (spirit_po::default_plural_forms::expr, e3))
 
-#define X_(name, op) \
+#define ADAPT_STRUCT_(name, op) \
 BOOST_FUSION_ADAPT_STRUCT(spirit_po::default_plural_forms:: name, \
   (spirit_po::default_plural_forms::expr, e1) \
   (spirit_po::default_plural_forms::expr, e2)) \
 
-SPIRIT_PO_BINARY_OPS_LIST
-#undef X_
+FOREACH_SPIRIT_PO_BINARY_OP(ADAPT_STRUCT_)
+
+#undef ADAPT_STRUCT_
 
 // X macros not used anymore
-#undef SPIRIT_PO_BINARY_OPS_LIST
+#undef FOREACH_SPIRIT_PO_BINARY_OP
 
 namespace spirit_po {
 
