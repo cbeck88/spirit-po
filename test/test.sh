d15738f8e395bf3e5697147df42ca6f34909cdd9
Chris Beck <beck.ct@gmail.com>
2016-11-25 09:34:46 -0500

    fixup travis (install boost.build, fixup b2 vs bjam)

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 rm -rf stage
-b2
+
+if hash b2 2>/dev/null; then
+  b2 "$@"
+elif hash bjam 2>/dev/null; then
+  bjam "$@"
+else
+  echo >&2 "Require b2 or bjam but it was not found. Aborting."
+  exit 1
+fi
+
 gdb -return-child-result -batch -ex "run" -ex "thread apply all bt" -ex "quit" --args ./stage/main
